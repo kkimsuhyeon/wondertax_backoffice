5f505beb22698b37c576aa7418afb98dd4532f56
kkimsuhyeon <wzxy2002@naver.com>
2021-03-11 19:58:46 +0900

    question/basic / 작업중

--- src/pages/question/QuestionRouter.tsx
+++ src/pages/question/QuestionRouter.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import BasicList from './basic/List';
+import BasicRegist from './basic/Regist';
+import BasicDetail from './basic/Detail';
 import BlankList from './blank/List';
 import OXList from './OX/List';
 
@@ -10,6 +12,8 @@ function QuestionRouter() {
     <BrowserRouter basename='/question'>
       <Switch>
         <Route exact path='/basic' component={BasicList} />
+        <Route exact path='/basic/regist' component={BasicRegist} />
+        <Route exact path='/basic/:basicId' component={BasicDetail} />
         <Route exact path='/blank' component={BlankList} />
         <Route exact path='/ox' component={OXList} />
         <Redirect to='/basic' />
