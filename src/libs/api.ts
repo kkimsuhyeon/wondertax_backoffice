c6758b23a57693fa3096f179ea12927eeabbdd75
kkimsuhyeon <wzxy2002@naver.com>
2021-03-13 13:02:30 +0900

    Api 테스트

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,9 +1,47 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export const BASE_URL = 'https://wondertax.du.r.appspot.com';
 
 const instance = axios.create({
   baseURL: BASE_URL,
 });
+
+instance.interceptors.response.use(
+  (response) => response,
+  (err) => err
+);
+
+export interface CreateRequestArguments {
+  endpoint: AxiosRequestConfig['url'];
+  method: AxiosRequestConfig['method'];
+  body?: { [key: string]: string | number | Blob | Array<string> | null };
+  params?: { [key: string]: string | number | Array<string> | undefined };
+  headers?: AxiosRequestConfig['headers'];
+}
+
+export interface RequestFunction<T = void, U = void> {
+  (args: T): Promise<U>;
+}
+
+export const createRequest = async <T = void>({
+  endpoint,
+  method,
+  headers = undefined,
+  body = undefined,
+  params = undefined,
+}: CreateRequestArguments): Promise<T> => {
+  try {
+    const { data } = await instance({
+      method,
+      url: `${endpoint}/${params}`,
+      data: body,
+      headers,
+    });
+    return data.data as T;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
 
 export default instance;
--- src/pages/question/basic/Regist.tsx
+++ src/pages/question/basic/Regist.tsx
@@ -1,17 +1,106 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
+import styled from '@emotion/styled';
+// import { css } from '@emotion/react';
 
 import instance from 'libs/api';
 
+// type은 뭘까?
+// createdOn은 생성날짜인데 내가 직접 적어주는건 아니겠지?
+// authorId는 일단 내가 전달해야하나?
+// unit은 단원인거 같다
+// difficultly: 난이도
+// question: 문제
+// choices: 보기
+// answerIdx: 답 번호
+// suffle 문제 섞을지
+
+const requestSubmit = async ({
+  type,
+  unit,
+  difficulty,
+  question,
+  choices,
+  answerIdx,
+  suffle,
+}: {
+  type?: string;
+  unit?: Array<number>;
+  difficulty?: number;
+  question?: string;
+  choices?: Array<string>;
+  answerIdx?: number;
+  suffle?: boolean;
+}) => {
+  await instance({
+    url: '/api/problem',
+    method: 'POST',
+    data: {
+      type: type,
+      unit: unit,
+      difficulty: difficulty,
+      question: question,
+      choices: choices,
+      answerIdx: answerIdx,
+      suffle: suffle,
+    },
+  });
+};
+
 function Regist() {
-  const handleTest = useCallback(() => {
-    instance({ url: '/api/problem', method: 'GET' });
+  const [type, setType] = useState<string>();
+  const [difficult, setDifficult] = useState<number>();
+  const [question, setQuestion] = useState<string>();
+  const [answerNumber, setAnswerNumber] = useState<number>();
+
+  const handleTypeChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setType(e.target.value);
   }, []);
 
-  useEffect(() => {
-    handleTest();
-  }, [handleTest]);
+  const handleQuestionChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuestion(e.target.value);
+  }, []);
 
-  return <div>regist</div>;
+  const handleDifficultChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setDifficult(Number(e.target.value));
+  }, []);
+
+  const handleAnswerChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setAnswerNumber(Number(e.target.value));
+  }, []);
+
+  const handleSubmit = useCallback(async () => {
+    await requestSubmit({
+      type: type,
+      answerIdx: answerNumber,
+      difficulty: difficult,
+      question: question,
+      choices: ['문제1', '문제2', '문제3', '문제4'],
+      suffle: false,
+      unit: [1, 1, 1, 1],
+    });
+  }, [type, answerNumber, difficult, question]);
+
+  return (
+    <Wrapper>
+      <div>타입</div>
+      <input value={type} onChange={handleTypeChange} />
+
+      <div>난이도</div>
+      <input value={difficult} onChange={handleDifficultChange} />
+
+      <div>문제</div>
+      <input value={question} onChange={handleQuestionChange} />
+
+      <div>답변</div>
+      <input value={answerNumber} onChange={handleAnswerChange} />
+
+      <button onClick={handleSubmit}>제출</button>
+    </Wrapper>
+  );
 }
 
 export default Regist;
+
+const Wrapper = styled.div`
+  max-width: 90rem;
+`;
