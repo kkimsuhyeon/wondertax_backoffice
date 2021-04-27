5c7d7613b0e6d3d5166c4ab2171c3bea113b3348
kkimsuhyeon <wzxy2002@naver.com>
2021-04-27 17:39:40 +0900

    Table Component 작성

--- src/components/common/Table.tsx
+++ src/components/common/Table.tsx
@@ -1,7 +1,85 @@
 import React from 'react';
 
-function Table() {
-  return <div>table</div>;
+import styled, { css } from 'styled-components';
+
+export interface Structure<T>
+  extends Array<{
+    id: keyof T;
+    title: string;
+    flex: string;
+    parser?: (args: Partial<T>) => React.ReactNode;
+  }> {}
+
+export interface PropTypes<T> {
+  structure: Structure<T>;
+  list?: Array<T>;
+  tableKey: keyof T;
+  onClick?: (args: T) => void;
+}
+
+function Table<T>({ structure, list = [], tableKey, onClick }: PropTypes<T>) {
+  return (
+    <Wrapper>
+      <Header>
+        {structure.map((item) => {
+          return (
+            <Data key={`${item.id}`} flex={item.flex}>
+              {item.title}
+            </Data>
+          );
+        })}
+      </Header>
+      <Body>
+        {list.map((item) => (
+          <Row key={`${item[tableKey]}`} onClick={onClick ? () => onClick(item) : undefined}>
+            {structure.map(({ flex, id, parser }) => (
+              <Data key={`${item[tableKey]}-${id}`} flex={flex}>
+                {parser ? parser(item) : item[id]}
+              </Data>
+            ))}
+          </Row>
+        ))}
+      </Body>
+    </Wrapper>
+  );
 }
 
 export default Table;
+
+const Wrapper = styled.div`
+  text-align: center;
+  border-top: 1px solid ${({ theme }) => theme.blackGray};
+  border-left: 1px solid ${({ theme }) => theme.blackGray};
+  background-color: white;
+`;
+
+const Row = styled.div<{ onClick?: () => void }>`
+  display: flex;
+  ${({ onClick, theme }) =>
+    css`
+      ${onClick &&
+      css`
+        cursor: pointer;
+        &:hover {
+          background-color: ${theme.lemon};
+        }
+      `}
+    `}
+`;
+
+const Header = styled(Row)`
+  ${({ theme }) => css`
+    background-color: ${theme.whiteGray};
+  `}
+`;
+
+const Body = styled.div``;
+
+const Data = styled.div<{ flex: string }>`
+  ${({ flex, theme }) => css`
+    border-right: 1px solid ${theme.blackGray};
+    border-bottom: 1px solid ${theme.blackGray};
+    padding: 1rem 0 1rem;
+    flex: ${flex};
+  `}
+`;
