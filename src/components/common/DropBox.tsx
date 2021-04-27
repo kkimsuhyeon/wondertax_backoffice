df92eb3fba63b25835401502afa20b187681aab4
kkimsuhyeon <wzxy2002@naver.com>
2021-04-27 15:49:53 +0900

    dropbox 수정

--- src/components/common/DropBox.tsx
+++ src/components/common/DropBox.tsx
@@ -1,140 +1,154 @@
-import React, { useState, useMemo, useCallback } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import styled, { css } from 'styled-components';
 
-export interface DataType {
+export interface DropboxItem {
   name: string;
   value: string;
 }
 
-interface WrapperProps {
+interface WrapperPropTypes {
   width?: string;
-  height?: string;
-  margin?: string;
   disabled?: boolean;
   selected?: string;
+  margin?: string;
 }
 
-export interface PropTypes extends Omit<WrapperProps, 'selected'> {
-  onChange: (value: DataType) => void;
-  list: Array<DataType>;
-  placeholder?: string;
-  value: string;
-  height?: string;
+export interface PropTypes extends Omit<WrapperPropTypes, 'selected'> {
+  list: Array<DropboxItem>;
+  value?: DropboxItem['value'];
+  onChange: (value: DropboxItem) => void;
   count?: string;
-  className?: string;
+  placeholder?: string;
+  height?: string;
 }
 
-const Wrapper = styled.div<WrapperProps>`
-  ${({ ...props }) =>
-    css`
-      user-select: none;
-      cursor: pointer;
-      position: relative;
-      width: ${props.width ?? `100%`};
-      ${props.margin && `margin: ${props.margin};`}
-      background-color: white;
-      border-radius: 4px;
-      font-size: 14px;
-      & > div > div:first-child {
-        ${!props.selected && `color: #E1E1E1;`}
-      }
-      & li {
-        padding: 0 10px;
-        width: 100%;
-        height: 2.5rem;
-        line-height: 2.5rem;
-        cursor: pointer;
-        &[data-value='${props.selected}'] {
-          background-color: '#EF9CAC';
-        }
-        &:hover {
-          background-color: '#EF9CAC';
-        }
-      }
-      ${props.disabled &&
-      css`
-        cursor: not-allowed;
-        background-color: '#EF9CAC';
-      `}
-    `}
-  &:focus {
-    outline: none;
-  }
-`;
-
-const Selector = styled.div<{ height?: string }>`
-  ${({ height }) => css`
-    height: ${height ?? `52px`};
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    border: 1px solid #16191d;
-    border-radius: 4px;
-    padding: 0px 17px;
-    @media screen and (max-width: 500px) {
-      font-size: 14px;
-      height: ${height ?? '46px'};
-    }
-  `}
-`;
-
-const List = styled.ul<{ open?: boolean; count?: string }>`
-  ${({ open, count = '4' }) => css`
-    ${!open && `display: none;`}
-    overflow-y: auto;
-    position: absolute;
-    width: 100%;
-    border: 1px solid #16191d;
-    border-top: 0;
-    height: ${`${Number(count) * 2.5}rem`};
-    background-color: #fff;
-    &::-webkit-scrollbar {
-      width: 0.3rem;
-      background: rgba(0, 0, 0, 0.05);
-      &-thumb {
-        background-color: '#AAAAAA';
-        border-radius: 5px;
-      }
-    }
-  `}
-`;
-
-function DropBox({ width, margin, height, onChange, disabled, list, placeholder, value, count }: PropTypes) {
-  const [isOpen, setOpen] = useState(false);
-
-  const toggleOpen = useCallback(() => {
-    if (!disabled) setOpen((prev) => !prev);
-  }, [disabled]);
+function DropBox({ list, count = '4', placeholder, height = '2.5rem', width = '100%', disabled = false, onChange, margin }: PropTypes) {
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
+  const [{ name, value }, setSelectValue] = useState<DropboxItem>({ name: '', value: '' });
 
   const handleClick = useCallback(
-    ({ name, value }: DataType) => {
-      onChange({ name, value });
-      setOpen(false);
+    ({ name, value }) => {
+      setSelectValue({ name: name, value: value });
+      onChange({ name: name, value: value });
+      wrapperRef.current?.blur();
     },
     [onChange]
   );
 
-  const selectedName = useMemo(() => list.find((item) => item.value === value)?.name ?? placeholder, [list, value, placeholder]);
+  const handleWrapperClick = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
+    if (document.activeElement === e.currentTarget) {
+      e.preventDefault(); // mouseDown은 Focus를 일으키는 이벤트임.
+      // 즉 mouseDown의 기본 이벤트를 막게되면 Focus를 막는다.
+      e.currentTarget.blur();
+    }
+  }, []);
 
-  const makeList = useMemo(
-    () =>
-      list.map(({ name, value }) => (
-        <li key={value} data-value={value} onClick={() => handleClick({ name, value })}>
-          {name}
-        </li>
-      )),
-    [handleClick, list]
-  );
   return (
-    <Wrapper width={width} margin={margin} disabled={disabled} selected={value} tabIndex={0} onBlur={() => setOpen(false)}>
-      <Selector height={height} onClick={toggleOpen}>
-        <div>{selectedName}</div>
-        <div style={{ fontSize: '0.5rem' }}>{isOpen ? '▲' : '▼'}</div>
-      </Selector>
-      <List open={isOpen} count={count}>
-        {makeList}
+    <Wrapper
+      tabIndex={0}
+      ref={wrapperRef}
+      onMouseDown={handleWrapperClick}
+      width={width}
+      disabled={disabled}
+      selected={value}
+      margin={margin}
+    >
+      <Selector height={height}>{value === '' ? placeholder : name}</Selector>
+      <List count={count}>
+        {list.map(({ name, value }) => (
+          <div
+            key={value}
+            data-value={value}
+            onMouseDown={(e) => e.stopPropagation()}
+            // 이벤트 전파 방지.
+            onClick={() => handleClick({ name: name, value: value })}
+          >
+            {name}
+          </div>
+        ))}
       </List>
     </Wrapper>
   );
 }
+
 export default DropBox;
+
+const Selector = styled.div<{ height: string }>`
+  position: relative;
+  height: ${({ height }) => height};
+  line-height: ${({ height }) => height};
+  text-align: center;
+  cursor: pointer;
+  border: 1px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.white};
+
+  &::after {
+    content: '▼';
+    position: absolute;
+    right: 5px;
+    font-size: 0.8rem;
+  }
+`;
+
+const List = styled.div<{ count: string; isOpen?: boolean }>`
+  ${({ theme, count }) => css`
+    position: absolute;
+    display: none;
+    overflow-y: auto;
+    height: ${`${count === '1' ? `2.54rem` : +count * 2.52}rem`};
+    width: 100%;
+    border: 1px solid black;
+    border-top: none;
+    background-color: ${theme.white};
+  `}
+
+  & > div {
+    text-align: center;
+    height: 2.5rem;
+    line-height: 2.5rem;
+    cursor: pointer;
+  }
+`;
+
+const Wrapper = styled.div<WrapperPropTypes>`
+  ${({ disabled, width, theme, selected }) => css`
+    position: relative;
+    user-select: none;
+    width: ${width};
+
+    & > ${List} > div[data-value='${selected}'] {
+      background-color: ${theme.lemon};
+    }
+
+    &:focus {
+      & > ${List} {
+        display: block;
+      }
+
+      & > ${Selector}::after {
+        content: '▲';
+      }
+    }
+
+    ${disabled &&
+    css`
+      & > ${Selector} {
+        cursor: not-allowed;
+        background-color: ${theme.whiteGray};
+      }
+
+      &:focus {
+        & > ${List} {
+          display: none;
+        }
+
+        & > ${Selector}::after {
+          content: '▼';
+        }
+      }
+    `}
+  `}
+`;
